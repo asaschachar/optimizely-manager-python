56e69c17f634fabc0dfe03704316ac0e47bb1329
Asa Schachar <asa@optimizely.com>
2019-03-12 11:29:46 -0700

    Update __init__.py

--- optimizely_manager/__init__.py
+++ optimizely_manager/__init__.py
@@ -20,22 +20,20 @@ class _UinintializedClient():
 
       If you just started a web application or app server, try the request again.
 
+      OR use the optimizely.fetch_configuration(timeout_ms=500) method to block initialization until the application is ready
       OR try moving your OptimizelyManager initialization higher in your application startup code
       OR move your is_feature_enabled call later in your application lifecycle.
 
       If this error persists, contact Optimizely!
 
-      TODO: Enable a blocking for Optimizely through the manager
     """
-    logger.log(logging.DEBUG, UNIINITIALIZED_ERROR)
+    logger.log(logging.INFO, UNIINITIALIZED_ERROR)
 
 
 class _OptimizelyManagerSingleton:
   def __init__(self, sdk_key=None, log_level=None, **kwargs):
     self._timer = None
-    self.interval = 1 # TODO: Provide configurability
     self.is_running = False
-    self.start()
     self.current_datafile = {}
     self.sdk_key = sdk_key
     self.log_level = log_level or logging.DEBUG
@@ -51,7 +49,7 @@ class _OptimizelyManagerSingleton:
     latest_datafile_string = json.dumps(latest_datafile, sort_keys=True)
 
     if current_datafile_string != latest_datafile_string:
-      self.logger.log(logging.DEBUG, 'MANAGER: Received an updated datafile and is re-initializing')
+      self.logger.log(logging.INFO, 'MANAGER: Received an updated datafile and is initializing')
       self.current_datafile = latest_datafile
 
       # The datafile is different! Let's re-instantiate the client
@@ -60,15 +58,14 @@ class _OptimizelyManagerSingleton:
         logger=self.logger,
         **self.sdkParameters
       )
-
   def _run(self):
     self.is_running = False
     self.start()
     self.request_datafile()
 
-  def start(self):
+  def start(self, interval=1):
     if not self.is_running:
-      self._timer = Timer(self.interval, self._run)
+      self._timer = Timer(interval, self._run)
       self._timer.start()
       self.is_running = True
 
@@ -78,7 +75,7 @@ class _OptimizelyManagerSingleton:
 
   def is_feature_enabled(self, feature_key, user_id=None):
     if not user_id:
-      self.logger.log(logging.DEBUG, 'MANAGER: No user_id supplied to is_feature_enabled, using random string instead.')
+      self.logger.log(logging.INFO, 'MANAGER: No user_id supplied to is_feature_enabled, using random string instead.')
       user_id = user_id or str(random.randint(1, 100))
 
     result = self.optimizely_client_instance.is_feature_enabled(feature_key, user_id)
@@ -87,14 +84,17 @@ class _OptimizelyManagerSingleton:
   def get_client(self):
     return self
 
-  def fetchConfiguration(self, timeout=500):
-    pass
+  def fetch_configuration(self, timeout_ms=500):
+    self.logger.log(logging.INFO, 'MANAGER: Blocking fetch for feature configuration')
+    self.request_datafile()
 
-  def startPollingThread(self, update_interval=1000):
-    pass
+  def start_polling_thread(self, update_interval_sec=1):
+    self.logger.log(logging.INFO, 'MANAGER: Starting background thread to poll for feature configuration updates')
+    self.start(update_interval_sec)
 
-  def startLiveUpdates(self, update_interval=1000):
-    pass
+  def start_live_updates(self, update_interval_sec=1):
+    self.logger.log(logging.INFO, 'MANAGER: Starting background thread to poll for feature configuration updates')
+    self.start(update_interval_sec)
 
 
 class OptimizelyManager:
@@ -108,12 +108,11 @@ class OptimizelyManager:
   def is_feature_enabled(self, feature_key, user_id=None):
     return OptimizelyManager.instance.is_feature_enabled(feature_key, user_id=user_id)
 
-  def fetchConfiguration(self, timeout=500):
-    pass
+  def fetch_configuration(self, timeout_ms=500):
+    return OptimizelyManager.instance.fetch_configuration(timeout_ms=timeout_ms)
 
-  def startPollingThread(self, update_interval=1000):
-    pass
+  def start_polling_thread(self, update_interval_sec=1):
+    return OptimizelyManager.instance.start_polling_thread(update_interval_sec=update_interval_sec)
 
-  def startLiveUpdates(self, update_interval=1000):
-    pass
-
+  def start_live_updates(self, update_interval_sec=1):
+    return OptimizelyManager.instance.start_live_updates(update_interval_sec=update_interval_sec)
